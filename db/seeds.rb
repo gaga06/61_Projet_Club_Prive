7321610b6b37cf00ea733765f3f3c6378cc7dba1
Gaelle <gaelle.gorgori@gmail.com>
2018-08-06 19:28:57 +0200

    ajout scaffold user et session

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+gaga = User.create!(first_name: 'ga', last_name: 'gor', email: 'gaga@gaga.com', password: 'gagaga', password_confirmation: 'gagaga')
